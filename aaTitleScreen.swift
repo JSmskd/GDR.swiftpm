52c9fcd99827a591baa666b341b620dea5b9ae0e
Evan Gross <egross6142@stu.d214.org>
2024-04-19 15:02:22 -0500

    Maid main menu pretty

--- aaTitleScreen.swift
+++ aaTitleScreen.swift
@@ -3,10 +3,29 @@ import SwiftUI
 struct aaTitleScreen: View {
     var body: some View {
         VStack {
-             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Group{
+                Text("Google Dino Game Renovated")
+                    .font(.custom("Zapfino", size: 15))
+                Text("Version - 0.01")
+            }
+            .font(.system(size: 25))
+            
+            Spacer()
+            Button(action: {
+                print("I wanted the button to do something")
+            }, label: {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .frame(maxWidth: 400, maxHeight: 100)
+                    .foregroundStyle(.black)
+                    .overlay(
+                Text("Start Game")
+                    .foregroundStyle(.white
+                                    )
+                )
+            })
+            Spacer()
+            
+            
         }
     }
 }
