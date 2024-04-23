781489c1c3fae083fb3fc21ab52d1074576f6275
Evan Gross <egross6142@stu.d214.org>
2024-04-23 14:21:39 -0500

    Title page done (for now)

--- StatsPage.swift
+++ StatsPage.swift
@@ -7,12 +7,11 @@
 
 import SwiftUI
 
-struct EquipmentPage: View {
+struct StatsPage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Stats")
+        
+        
     }
 }
 
-#Preview {
-    EquipmentPage()
-}
