e235c13f65c519223a18613be37b3a3c65debb06
John Sencion <jsencion7366@stu.d214.org>
2024-05-03 15:02:20 -0500

    I don't even know

--- InfiniteRunnerGame.swift
+++ InfiniteRunnerGame.swift
@@ -3,6 +3,7 @@
     //  GDR
     //
     //  Created by Evan Gross on 4/19/24.
+    //  Built by John Sencion 5/1/24.
     //
 
 import SwiftUI
@@ -12,6 +13,9 @@ struct InfiniteRunnerGame: View {
     var body: some View {
         GeometryReader(content: { geometry in
             SpriteView(scene: GameScene(size: geometry.size))
+        })
+        .onAppear(perform: {
+            binaryPos().understant( binaryPos().decode(binaryPos().encode(false, false, false, true)))
         })
 
     }//
@@ -24,8 +28,22 @@ struct InfiniteRunnerGame: View {
 
 class GameScene: SKScene{
     let floorO:CGFloat = 32
-    var flip:SKShapeNode = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 0, height: 0))
-
+    let bsw:CGFloat = 25//base size width
+    let ewr:CGFloat = 1.3//width random
+    let bsh:CGFloat = 40//base size height
+    let ehr:CGFloat = 1.23//height random
+    let enem:[SKSpriteNode] = [
+        SKSpriteNode(color: .red, size: CGSize(width: 0, height: 0))
+    ]
+    func rando(base:CGFloat, ofs:CGFloat,_ oop:Bool?)->CGFloat{
+        if oop == Optional(nil){
+            return base + (CGFloat.random(in: -1...1) * ofs * base * -1 + base)
+        }
+        //print(oop!)
+        return base + ((oop! ? 1 : -1) * ofs * base * -1 + base)
+    }
+    let phb = SKSpriteNode(color: .red, size: CGSize(width: 0, height: 0))
+var flip = SKSpriteNode(color: .blue, size: CGSize(width: 20, height: 20))//floor
     @State var st:CGFloat = 0
 
     let hightM:CGFloat = 270
@@ -43,15 +61,21 @@ class GameScene: SKScene{
     let ahh = SKSpriteNode(imageNamed: "ahh")
     let data = SKSpriteNode(color: .red, size: CGSize(width: 20, height: 20))
     override func sceneDidLoad() {
+        self.phb.color = .red
+        //phb.
+        phb.size = CGSize(width: bsw * ewr, height: bsh * ehr)
         self.backgroundColor = .white
-        self.flip = SKShapeNode(rect: CGRect(x: 0, y: 0, width: size.width, height: floorO))
+        self.flip.color = .blue
         self.addChild(flip)
-        flip.fillColor = .blue
+        self.flip.position = CGPoint(x: size.width / 2, y: self.floorO / 2)
+        self.flip.size = CGSize(width: size.width, height: self.floorO)
+        //flip.fillColor = .blue
             //self.fax floorO
 
         self.addChild(foo)
         self.addChild(data)
         self.addChild(foo2)
+        self.addChild(enem[0])
 
         foo.size = CGSize(width: sz["width"]!, height: sz["height"]!) //64/48 //CGPath(rect: CGRect(x: 0, y: 0, width:foo.size.width, height:foo.size.height),transform:) //= CGSize()
         foo2.size = foo.size
@@ -105,7 +129,7 @@ class GameScene: SKScene{
 
             //grounded falling jumping
         let state:Int = Int(data.position.y == 0 ? 0 :(data.position.y == 2 || data.position.y == 4 ? 1 : (data.position.y == 1 || data.position.y == 3 ? 2 : 3)))
-        print(state)
+        //print(state)
         if state == 1 {
             foo.position.y -= fall
             if foo.position.y < (foo.size.height / 2 + floorO) {
@@ -150,8 +174,44 @@ class GameScene: SKScene{
 
             //-(yy-1)
         foo2.position = Int(currentTime * 10 / 5 * 4) % 2 == 0 ? foo.position : CGPoint(x: -1.0, y: -1.0)//this is the same line as above
+        var oop = false
+        var i = 0
+        while i < enem.count{
+            if enem[i].position.x < 0{
+                enem[0].position = CGPoint(x: size.width, y: floorO)
+                enem[0].size = CGSize(width: rando(base: bsw, ofs: ewr, oop), height: rando(base: bsh, ofs: ehr, oop))
+                oop.toggle()
+            }
+            enem[0].position.x -= 10
+            i += 1
+        }
                                                                                                          //(self.st)
     }
 }
 
-
+struct binaryPos {
+    //◽◾
+    func encode(_ grounded:Bool, _ rising:Bool, _ pressing:Bool,_ extraBit:Bool)->CGFloat  {
+        return (grounded ? 1 : 0) + (rising ? 2 : 0) + (pressing ? 4 : 0) + (extraBit ? 8 : 0)
+    }
+    func decode(_ Val:CGFloat)->[Bool]{
+var    out:[Bool]=[]
+        var x = 0
+        var y = 8
+        while x < 4{
+            if Int(Val) >= y{
+                out.append(true)
+            }
+            else {
+                out.append(true)
+            } 
+            y /= 2
+            x += 1
+        }
+    }
+    func understant(_ data:[Bool]){
+        //print("\()\()\()\()")
+        //print()
+        //print()
+    }
+}
