2a4bab1f00ad62d731f1cad0ab4edb3281410f34
Evan Gross <egross6142@stu.d214.org>
2024-05-15 15:21:56 -0500

    lootbox :o

---
+++ InventoryPage.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryPage.swift
+//  GDR
+//
+//  Created by Evan Gross on 5/15/24.
+//
+
+import SwiftUI
+
+struct InventoryPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InventoryPage()
+}
